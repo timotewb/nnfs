a794805c85b3ac4f014f68c53b01049ce5477795
timotewb <timote.wb@gmail.com>
2024-01-18 21:06:39 +1300

    go dev

--- go/app/0201_HiddenLayers.go
+++ go/app/0201_HiddenLayers.go
@@ -3,12 +3,23 @@ package app
 import "fmt"
 
 // 3 neurons
-type Layer1 struct {
+type Layers struct {
+	InputLayer InputLayer
+	HiddenLayer []HiddenLayer
+	OutputLayer OutputLayer
+}
+// 3 neurons
+type InputLayer struct {
 	Weights [3][4]float32 `json:"weights"`
 	Bias [3]float32 `json:"bias"`
 }
 // 3 neurons
-type Layer2 struct {
+type HiddenLayer struct {
+	Weights [3][3]float32 `json:"weights"`
+	Bias [3]float32 `json:"bias"`
+}
+// 3 neurons
+type OutputLayer struct {
 	Weights [3][3]float32 `json:"weights"`
 	Bias [3]float32 `json:"bias"`
 }
@@ -20,18 +31,30 @@ func HiddenLayers(){
 	{2,5,-1,2},
 	{-1.5,2.7,3.3,-.8}}
 
-	var layer1 Layer1
+	var layer1 InputLayer
 	layer1.Weights[0] = [4]float32{0.2, 0.8, -0.5, 1}
 	layer1.Weights[1] = [4]float32{0.5, -0.91, 0.26, -0.5}
 	layer1.Weights[2] = [4]float32{-0.26, -0.27, 0.17, 0.87}
 
 	layer1.Bias = [3]float32{2, 3, 0.5}
 
-	var layer2 Layer2
+	var layer2 OutputLayer
 	layer2.Weights[0] = [3]float32{0.1, -0.14, 0.5}
 	layer2.Weights[1] = [3]float32{-0.5, 0.12, -0.33}
 	layer2.Weights[2] = [3]float32{-0.44, 0.73, -0.13}
 
 	layer2.Bias = [3]float32{-1, 2, -0.5}
+
+	var layers Layers
+	layers.InputLayer = layer1
+	layers.OutputLayer = layer2
+
+	// calcualte input layer
+	for i:=0 ; i<len(inputs) ; i++ {
+		fmt.Println(inputs[0][i])
+	}
+
+	// we run one row of input through the entire model and save to array, then run next input
+
 
 }
