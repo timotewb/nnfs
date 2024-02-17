64d251bfb45118e57b3ea61dd97003086cc56e88
timotewb <timote.wb@gmail.com>
2024-02-18 10:07:54 +1300

    hidden layer dev

--- go/app/0201_HiddenLayers.go
+++ go/app/0201_HiddenLayers.go
@@ -7,6 +7,7 @@ type Layers struct {
 	InputLayer   InputLayer
 	HiddenLayers []HiddenLayer
 	OutputLayer  OutputLayer
+	layerOutput  []float32
 }
 
 // 3 neurons
@@ -41,69 +42,82 @@ func HiddenLayers() {
 
 	layer1.Bias = [3]float32{2, 3, 0.5}
 
-	// no hidden layer
-
-	var layer2 OutputLayer
+	var layer2 HiddenLayer
 	layer2.Weights[0] = [3]float32{0.1, -0.14, 0.5}
 	layer2.Weights[1] = [3]float32{-0.5, 0.12, -0.33}
 	layer2.Weights[2] = [3]float32{-0.44, 0.73, -0.13}
 
 	layer2.Bias = [3]float32{-1, 2, -0.5}
 
+	var layer3 HiddenLayer
+	layer3.Weights[0] = [3]float32{0.1, -0.14, 0.5}
+	layer3.Weights[1] = [3]float32{-0.5, 0.12, -0.33}
+	layer3.Weights[2] = [3]float32{-0.44, 0.73, -0.13}
+
+	layer3.Bias = [3]float32{-1, 2, -0.5}
+
+	var layer4 OutputLayer
+	layer4.Weights[0] = [3]float32{0.1, -0.14, 0.5}
+	layer4.Weights[1] = [3]float32{-0.5, 0.12, -0.33}
+	layer4.Weights[2] = [3]float32{-0.44, 0.73, -0.13}
+
+	layer4.Bias = [3]float32{-1, 2, -0.5}
+
 	var layers Layers
 	layers.InputLayer = layer1
-	layers.OutputLayer = layer2
+	layers.HiddenLayers = []HiddenLayer{layer2, layer3}
+	layers.OutputLayer = layer4
 
 	var n int
-	var w int
 	var b int
 	var h int
+	var i int
 	var v float32
 	// for each batch (array in inputs) run through model and save
 	for b = 0; b < len(inputs); b++ {
-		fmt.Println("\nBatch:", inputs[b])
 		//----------------------------------------------------------------------------------------
 		// input layer
 		//----------------------------------------------------------------------------------------
-		layerOutput := make([]float32, len(layers.InputLayer.Weights))
+		layers.layerOutput = make([]float32, len(layers.InputLayer.Weights))
 		// for each neuron
-		for n = 0; n < len(layers.InputLayer.Weights); n++ {
-			// for each weight/input pair
+		for n = 0; n < len(layers.InputLayer.Bias); n++ {
+			// for each input multiply by weight
 			v = 0
-			for w = 0; w < len(layers.InputLayer.Weights[n]); w++ {
-				v = v + (inputs[b][n] * layers.InputLayer.Weights[n][w])
+			for i = 0; i < len(inputs[b]); i++ {
+				v = v + inputs[b][i]*layers.InputLayer.Weights[n][i]
 			}
-			layerOutput[n] = v
+			// add bias and append to output layer
+			layers.layerOutput[n] = v + layers.InputLayer.Bias[n]
 		}
 
 		//----------------------------------------------------------------------------------------
 		// hidden layers
 		//----------------------------------------------------------------------------------------
 		for h = 0; h < len(layers.HiddenLayers); h++ {
-			inputs := layerOutput
-			layerOutput := make([]float32, len(layers.HiddenLayers))
+			inputs := layers.layerOutput
+			layers.layerOutput = make([]float32, len(layers.HiddenLayers[h].Bias))
 			// for each neuron
-			for n = 0; n < len(layers.HiddenLayers[h].Weights); n++ {
+			for n = 0; n < len(layers.HiddenLayers[h].Bias); n++ {
 				v = 0
-				for w = 0; w < len(layers.HiddenLayers[h].Weights[n]); w++ {
-					v = v + (inputs[w] * layers.HiddenLayers[h].Weights[n][w])
+				for i = 0; i < len(inputs); i++ {
+					v = v + (inputs[i] * layers.HiddenLayers[h].Weights[n][i])
 				}
-				layerOutput[n] = v
+				layers.layerOutput[n] = v + layers.HiddenLayers[h].Bias[n]
 			}
 		}
 
 		//----------------------------------------------------------------------------------------
 		// output layer
 		//----------------------------------------------------------------------------------------
-		batchOutput := make([]float32, len(layers.OutputLayer.Weights))
+		batchOutput := make([]float32, len(layers.OutputLayer.Bias))
 		for n = 0; n < len(layers.OutputLayer.Weights); n++ {
 			v = 0
-			for w = 0; w < len(layers.OutputLayer.Weights[n]); w++ {
-				v = v + (layerOutput[w] * layers.OutputLayer.Weights[n][w])
+			for i = 0; i < len(layers.layerOutput); i++ {
+				v = v + (layers.layerOutput[i] * layers.OutputLayer.Weights[n][i])
 			}
-			batchOutput[n] = v
+			batchOutput[n] = v + layers.OutputLayer.Bias[n]
 		}
-		fmt.Println(layerOutput)
+		fmt.Println(" ", batchOutput)
 	}
 
 	// we run one row of input through the entire model and save to array, then run next input
